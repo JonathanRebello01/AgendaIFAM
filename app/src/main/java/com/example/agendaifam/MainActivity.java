0ba9d7e1ecaae594ae7bb0c220ef6bccbb65d42c
Jonathan <rebello.jonathan@gmail.com>
2024-11-19 18:46:32 -0400

    Telas de login, cadastro e Recuperar senha finalizadas

--- app/src/main/java/com/example/agendaifam/MainActivity.java
+++ app/src/main/java/com/example/agendaifam/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.agendaifam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button login = findViewById(R.id.button_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(it);
+            }
+        });
+
+
     }
 }
